e30877ca3802b4655453fb22d375c7db6cc82ba7
himani1717 <96773488+himani1717@users.noreply.github.com>
2022-05-15 22:58:52 +0530

    Update and rename insertion sort.cpp to insertion sort1.cpp

---
+++ assignment7/insertion sort1.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#include<climits>
+
+using namespace std;
+
+void printVector(vector<int>A)
+{
+    for(int a:A) cout<<a<<" ";
+    cout<<"\n";
+}
+ vector<int>insertionsort(vector<int>A)
+ {
+    cout<<"insertion sort" << endl;
+    for(int i=1;i<A.size();i++)
+    {
+       for(int j=i;j>0;j--)
+       {
+          if(A[j]<A[j-1])
+          {
+              swap(A[j],A[j-1]);
+          }
+          else break;
+       }
+       printVector(A);
+   }   
+   return A;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    int a;
+    vector<int>A;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a;
+        A.push_back(a);
+    }
+    insertionsort(A);
+}
