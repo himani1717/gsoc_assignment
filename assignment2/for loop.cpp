d97ca41c4a78a70a08a5984e9703bcbc5cf35d55
himani1717 <96773488+himani1717@users.noreply.github.com>
2022-05-19 09:22:32 +0530

    Update for loop.cpp

--- assignment2/for loop.cpp
+++ assignment2/for loop.cpp
@@ -1,23 +1,73 @@
 #include <bits/stdc++.h>
-#include <vector>
 using namespace std;
+int a,b;
 
-int main() {
-    int a,b;
-    cin>>a>>b;
-    string A[9]={"one","two","three","four","five","six","seven","eight","nine"};
-    for(int i=a;i<=b;i++){
-        if(i<=9){
-            cout<<A[i-1]<<endl;
+int main() 
+{
+    
+    
+    cin >> a >> b;
+    for(int i=a ;i<=b;i++) 
+    {
+        if(i>=1 && i<=9)
+        { 
+            
+            switch (i % 10)
+            {
+                case 0:
+                cout << "zero" << endl;
+                break;
+                
+                case 1:
+                cout << "one " << endl;
+                break;
+                
+                case 2:
+                cout << "two " << endl;
+                break;
+                
+                case 3:
+                cout << "three " << endl;
+                break;
+                
+                case 4:
+                cout << "four " << endl;
+                break;
+                
+                case 5:
+                cout << "five " << endl;
+                break;
+                
+                case 6:
+                cout << "six " << endl;
+                break;
+                
+                case 7:
+                cout << "seven " << endl;
+                break;
+                
+                
+                case 8:
+                cout << "eight " << endl;
+                break;
+                
+                case 9:
+                cout << "nine " << endl;
+                break;
+                 
+            }
         }
-        else if(i%2==0){
-            cout<<"even"<<endl;
-        }
-        else {
+        else if (i>9 && i%2==0)
         {
-            cout<<"odd"<<endl;
+            cout << "even" << endl;
+            
         }
+    
+        else
+        {
+            cout << "odd" << endl;
         }
-        
+   
     }
-    return 0;
+    
+}
