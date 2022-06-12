32f0225cf1956960c83be00f9d621fbaa45367d9
himani1717 <96773488+himani1717@users.noreply.github.com>
2022-06-12 19:01:37 +0530

    Update adding digits.cpp

--- assignment4/adding digits.cpp
+++ assignment4/adding digits.cpp
@@ -1,27 +1,15 @@
-#include<bits/stdc++.h>
-using namespace std;
-   
-int s=0;
-int Sum(int n)
-{
-        
-    cin >> n;
-    while(n>0 || s>9)
-    {
-        if(n == 0)
-        {
-            n = s;
-            s = 0;
-        }
-        s =s + (n%10);
-        n=n/10;
-    }
-    return s;
-}
-int main()
-{
-    int n;
+class Solution {
+public:
     
-    cout << Sum(n);
-    return 0;
-}
+int addDigits(int n) {
+    int sum=0;
+    while(n>0){
+        sum=sum+n%10;
+        n=n/10;
+        }
+    if(sum>9){
+            return addDigits(sum);
+    }
+        return sum;
+    }
+};
