927b7975bd344a0b7f68f7a3a7f9d2819a36ce5a
himani1717 <96773488+himani1717@users.noreply.github.com>
2022-06-24 09:40:17 +0530

    Update row column finding target.cpp

--- assignment17/row column finding target.cpp
+++ assignment17/row column finding target.cpp
@@ -19,14 +19,14 @@ bool isTargetPresentInCol(vector<vector<int> >A, int target, int col){
 int main() {
    
     vector<vector<int>>A={{5,8,1} , {7,3,2},{6,4,9}};
-     if(isTargetPresentInCol(A,9,2))
-        cout<<"Target is present in the specified column"<<endl;
-    else
-        cout<<"Target is not present in the specified column"<<endl;   
     if(isTargetPresentInRow(A,5,1))
         cout<<"Target is present in the specified row"<<endl;
     else
         cout<<"Target is not present in the specified row"<<endl;
+     if(isTargetPresentInCol(A,9,2))
+        cout<<"Target is present in the specified column"<<endl;
+    else
+        cout<<"Target is not present in the specified column"<<endl;   
   
     return 0;   
 }
