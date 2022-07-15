e378292b933d992973c25b35052ebd5e112cb106
himani1717 <96773488+himani1717@users.noreply.github.com>
2022-07-15 09:45:31 +0530

    Create preOrder tree traversal.cpp

---
+++ assignment 26/preOrder tree traversal.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    void preOrder(TreeNode* root,vector<int>&v){
+        if(root==NULL){
+            return ;
+        }
+        v.push_back(root->val);
+        preOrder(root->left,v);
+        preOrder(root->right,v);
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int>v;
+        preOrder(root,v);
+        return v;
+    }
+};
