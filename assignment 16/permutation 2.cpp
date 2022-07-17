cce00059f490eab5e60d4ee4d3959d04e80a7cfd
himani1717 <96773488+himani1717@users.noreply.github.com>
2022-07-17 22:42:10 +0530

    Create permutation 2.cpp

---
+++ assignment 16/permutation 2.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<int>> A;
+    bool Swap_(vector<int>& nums,int start,int curr){
+        for(int i=start; i<curr; i++)
+            if(nums[curr] == nums[i])
+                return false;
+        return true;
+    }
+    void permutation(vector<int>& nums,int x,int y){
+        if(x==y){
+            A.push_back(nums);
+        }
+        else{
+            for(int i=x; i<=y; i++){
+            if(Swap_(nums,x,i)){
+                swap(nums[x],nums[i]);
+                permutation(nums,x+1,y);
+                swap(nums[x],nums[i]);
+            }
+            }
+        }
+        
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        int n = nums.size()-1;
+        permutation(nums,0,n);
+        return A;
+    }
+};
