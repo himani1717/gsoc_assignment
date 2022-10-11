62a9a2baec45605339858924f3f7a1dd48958e2e
himani1717 <96773488+himani1717@users.noreply.github.com>
2022-10-11 12:02:11 +0530

    Create IsValidBST.cpp

---
+++ Assignment 27/IsValidBST.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool Validate(TreeNode* root, const long left,const long &right) {
+        if(root == NULL)
+            return 1;
+        if(!(root->val > left && root->val < right))
+            return 0;
+        return (Validate(root->left,left,root->val) && Validate(root->right,root->val,right));
+    }
+    bool isValidBST(TreeNode* root) {
+        if(root->left == NULL && root->right == NULL)
+            return 1;
+        return Validate(root,LONG_MIN,LONG_MAX);
+        
+     }
+};
