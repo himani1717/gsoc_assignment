f9a88b5ff26b19d7d704a16d15a9b755da01e5df
himani1717 <96773488+himani1717@users.noreply.github.com>
2022-11-29 10:31:31 +0530

    Create Excel Column Number.cpp

---
+++ Assignment 2(Maths) Interviewbit/Excel Column Number.cpp
@@ -0,0 +1,12 @@
+int Solution::titleToNumber(string A) {    
+    int size = A.size();
+    int s=0;
+    int j=size-1;
+    for( int i = 0; i <= size - 1; i++ ){
+       int x=A[i]-'A'+1;
+        s=s+pow(26,j)*x;
+        j--;
+    }
+    return s;
+    
+}
