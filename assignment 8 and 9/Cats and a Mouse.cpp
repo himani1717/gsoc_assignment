7a6345181fec77a1bb16e97a719d107bb54653d4
himani1717 <96773488+himani1717@users.noreply.github.com>
2022-12-19 12:26:14 +0530

    Create Cats and a Mouse.cpp

---
+++ assignment 8 and 9/Cats and a Mouse.cpp
@@ -0,0 +1,9 @@
+string catAndMouse(int x, int y, int z) {
+  if(abs(x-z)==abs(y-z)){
+      return "Mouse C";
+  }
+  else if(abs(x-z)>abs(y-z)){
+      return "Cat B";
+  }
+  else return "Cat A";
+}
